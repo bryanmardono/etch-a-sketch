20c8c6917791e7a954fd44a0b13a08368e20e96a
bryanmardono <bryanmardono1@gmail.com>
2024-01-22 15:43:25 +0800

    Create coloring function

--- script.js
+++ script.js
@@ -24,16 +24,16 @@ function setMode(newMode) {
 
 colorPicker.oninput = (e) => setColor(e.target.value); //change currentColor to whatever is chosen in the color picker
 colorBtn.onclick = () => setMode('color'); //changes currentMode to color
-eraserBtn.oncancel = () => setMode('eraser');
+eraserBtn.onclick = () => setMode('eraser');
 
 //function to create grids (div creation by for loop)
 function generateGrid(divSize = 16*16, className = 'grid-16') {
     grid.innerHTML = '';
-    for (let i = 0; i < divSize; i++) {
+    for (let i = 0; i < divSize; i+=1) {
         const gridDiv = document.createElement('div');
         //className is needed to determine number of grid columns in css through grid-template-columns
-        grid.classList.remove('grid-8', 'grid-16', 'grid-32');
-        grid.classList.add(className);
+        gridDiv.classList.remove('grid-8', 'grid-16', 'grid-32');
+        gridDiv.classList.add(className);
         grid.appendChild(gridDiv);
     }
     currentDivSize = divSize;
@@ -46,18 +46,27 @@ function chooseGrid() {
         button.addEventListener('click', () => {
             //active class needs to be removed to reset button styling to default
             button.classList.remove('active');
-            if (button.id = "grid-8") {
+            if (button.id == "grid-8") {
                 sizePicker[0].classList.add('active');
                 generateGrid(8*8, 'grid-8');
-            } else if (button.id = "grid-16") {
+            } else if (button.id == "grid-16") {
                 sizePicker[1].classList.add('active');
-                generateGrid();
-            } else if (button.id = "grid-32") {
+                generateGrid(16*16, 'grid-16');
+            } else if (button.id == "grid-32") {
                 sizePicker[2].classList.add('active');
                 generateGrid(32*32, 'grid-32');
             }
         });
     });
+}
+
+function colorGrid() {
+    if (e.type === 'mouseover' && !mouseDown) return;
+    if (currentMode === 'color') {
+        e.target.style.backgroundColor = currentColor;
+    } else if (currentMode === 'eraser') {
+        e.target.style.backgroundColor = '#fefefe';
+    }
 }
 
 function reloadGrid() {
@@ -66,5 +75,7 @@ function reloadGrid() {
 
 clearBtn.onclick = () => reloadGrid();
 
-chooseGrid();
-generateGrid();
+document.addEventListener('DOMContentLoaded', () => {
+    chooseGrid();
+    generateGrid();
+});
