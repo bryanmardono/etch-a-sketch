e0e7c1586a08ab757b785e464d65cd1a7515705c
bryanmardono <bryanmardono1@gmail.com>
2024-01-22 13:33:12 +0800

    Create color, mode, and grid size variable in javascript

---
+++ script.js
@@ -0,0 +1,7 @@
+const defaultColor = '#080808'; //black
+const defaultMode = 'color'; 
+const defaultGridSize = '16';
+
+let currentColor = defaultColor;
+let currentMode = defaultMode;
+let currentGridSize = defaultGridSize;
