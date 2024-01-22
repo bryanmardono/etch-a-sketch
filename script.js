0dd347fa06bf6d9848bce0a1440fe3923a058973
bryanmardono <bryanmardono1@gmail.com>
2024-01-22 14:07:52 +0800

    Create JS query selectors for DOM elements in html

--- script.js
+++ script.js
@@ -5,3 +5,11 @@ const defaultGridSize = '16';
 let currentColor = defaultColor;
 let currentMode = defaultMode;
 let currentGridSize = defaultGridSize;
+
+const colorPicker = document.querySelector('colorPicker');
+const colorBtn = document.querySelector('colorBtn');
+const eraserBtn = document.querySelector('eraserBtn');
+const clearBtn = document.querySelector('clearBtn');
+const sizeValue = document.querySelector('size-value');
+const sizePicker = document.querySelector('size-picker');
+const grid = document.querySelector('grid-container');
